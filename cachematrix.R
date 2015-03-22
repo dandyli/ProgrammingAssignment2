af48a715cb897fb10baeb76462e3f09e4d342ecf
dandyli <dandyli@users.noreply.github.com>
2015-03-22 23:58:19 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,12 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+# I just get stuck on it. T____T Difficult to solve that in one day. I even can not understand the example. So upset about that.
+# It seems that in the video the professor teaches me "1+1=2" and then the assignment shows "if y=ln(-6x^2+7x+3), what is y'?"
+# Whatever, I will try to solve it next week. Hope everything goes well. T________T
+# BTW: would you mind providing your code for me? (Just paste your link in the "feedback") 
+# I just wanna know whether my code is right in the future. Thank you so much for you nice help~:) 
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
